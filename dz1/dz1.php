7f2d412ea804125de202e3f85a92049c67661d1f
Alex Chernyakov <rustavellyne@gmail.com>
2018-12-06 17:37:53 +0200

    task49

--- dz1/dz1.php
+++ dz1/dz1.php
@@ -258,6 +258,16 @@ foreach ($arr_3 as $item)
 }
 echo "$result \n";
 
+//task48
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+
+foreach ($arr as $key => $value) {
+    echo "this is key = {$key} \n";
+}
+
+foreach ($arr as $key => $value) {
+    echo "this is value = {$value} \n";
+}
 
 
 echo "\n";
